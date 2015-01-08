d650e8751c944415b8caddc90d64b3cd336298c1
MinasMazar <minasmazar@gmail.com>
2015-01-08 15:41:57 +0100

    Implement Module convention feature

--- lib/ametista.rb
+++ lib/ametista.rb
@@ -2,6 +2,7 @@ require "ametista/version"
 require "ametista/iterators"
 require "ametista/strings"
 require "ametista/logger"
+require "ametista/modules"
 
 module Ametista
   # Your code goes here...
